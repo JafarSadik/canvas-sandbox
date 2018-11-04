38eb8d5cc76e1b35c4ee4eaae9758d63506897f3
Jafar Sadik <dzafar.sadik@gmail.com>
2018-11-04 12:53:43 +0000

    simplified exports from index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import {Vector2D} from "./Vector2D";
-import {FPS} from "./FPS"
-import {Colour} from "./Colour";
-
-export {Vector2D, FPS, Colour}
+export * from "./Vector2D";
+export * from "./FPS"
+export * from "./Colour";
